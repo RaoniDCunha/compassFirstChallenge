06336add9f248fc5df87f470628308b95716748c
drakkar95 <raonidutracunha@ymail.com>
2024-10-29 08:40:04 -0300

    feat: login page created

---
+++ scripts/login.js
@@ -0,0 +1,10 @@
+let button = document.getElementById("login-form-button");
+
+button.addEventListener('click', validateAndRedirect); 
+
+function validateAndRedirect(){
+    alert("ativou");
+    console.log("clicou")
+   // window.location.href = "http://logged_page.html";
+}
+
